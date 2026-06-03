160fb6d5498f7bfa9dcfb07bcd59506c4dffe94e
markaivey281@gmail.com <markaivey281@gmail.com>
2026-06-03 02:36:53 -0500

    Bigger but takes about the same amount of time with quantizer.

--- mha_cu/mha.cpp
+++ mha_cu/mha.cpp
@@ -238,7 +238,101 @@ void wide_mha_kernel(s_mfdata_v_t &xb,
 	}
 }
 
-void mha_kernel(mfdata_v_t *tokens, //6 mha_kernel
+void mha_kernel(hls::stream<my_float_t> &sf,
+								s_idata_v_t &w,
+								fdata_v_t *tokens, //6 mha_kernel
+                mfdata_v_t *key_cache, 
+                mfdata_v_t *value_cache, 
+								mfdata_v_t *key_cache_in, mfdata_v_t *value_cache_in,
+                const int POS, const int CURR_LAYER){
+  
+
+	constexpr int QUANT_DEPTH = MODEL_ELEMENTS * MODEL_ELEMENTS * MODEL_NUM_LAYERS / MAX_QUANT_ELEM;
+	constexpr int SF_DEPTH = MODEL_ELEMENTS * MODEL_ELEMENTS * MODEL_NUM_LAYERS / (MODEL_SCALING_FACTOR * SM_FL_ELEM);
+	constexpr int HD_QUANT_DEPTH = MODEL_HIDDEN_DIM * MODEL_ELEMENTS * MODEL_NUM_LAYERS  / MAX_QUANT_ELEM;
+	constexpr int HD_SF_DEPTH = MODEL_HIDDEN_DIM * MODEL_ELEMENTS * MODEL_NUM_LAYERS / (MODEL_SCALING_FACTOR * SM_FL_ELEM);
+	constexpr int CACHE_DEPTH = MODEL_ELEMENTS * MODEL_SEQUENCE_LEN * MODEL_NUM_LAYERS / MAX_FL_ELEM;
+	constexpr int TOK_DEPTH = MODEL_ELEMENTS / MAX_FL_ELEM;
+	constexpr int RMS_DEPTH = MODEL_ELEMENTS * MODEL_NUM_LAYERS / MAX_FL_ELEM;
+	constexpr	int LOGITS_QUANT_DEPTH = MODEL_ELEMENTS * MODEL_TOKENS / MAX_QUANT_ELEM;
+	constexpr int LOGITS_SF_DEPTH =  MODEL_ELEMENTS * MODEL_TOKENS / (MODEL_SCALING_FACTOR * SM_FL_ELEM);
+	constexpr int LOGITS_DEPTH = MODEL_TOKENS / MAX_FL_ELEM;
+
+	#pragma HLS INTERFACE mode=m_axi port=value_cache			bundle=vc_gemm		depth=CACHE_DEPTH			offset=slave max_read_burst_length=(4096/MAX_DW * 8)		max_write_burst_length=(4096/MAX_DW * 8)
+	#pragma HLS INTERFACE mode=m_axi port=key_cache				bundle=kc_gemm		depth=CACHE_DEPTH			offset=slave max_read_burst_length=(4096/MAX_DW * 8)		max_write_burst_length=(4096/MAX_DW * 8)
+	#pragma HLS INTERFACE mode=m_axi port=value_cache_in	bundle=token_gemm		depth=TOK_DEPTH			offset=slave max_read_burst_length=(4096/MAX_DW * 8)
+	#pragma HLS INTERFACE mode=m_axi port=key_cache_in		bundle=token_gemm		depth=TOK_DEPTH			offset=slave max_read_burst_length=(4096/MAX_DW * 8)
+	#pragma HLS INTERFACE mode=m_axi port=tokens					bundle=token_gemm	depth=TOK_DEPTH				offset=slave	max_read_burst_length=(4096/MAX_DW * 8)
+	// #pragma HLS INTERFACE mode=m_axi port=sf					bundle=token_gemm	depth=TOK_DEPTH				offset=slave	max_write_burst_length=(4096/MAX_DW * 8)  
+	// #pragma HLS INTERFACE mode=m_axi port=w					bundle=token_gemm	depth=TOK_DEPTH				offset=slave	max_write_burst_length=(4096/MAX_DW * 8) 
+	/* **********************************************************************************/
+	#pragma HLS INTERFACE mode=s_axilite port=tokens				 					bundle=control
+	#pragma HLS INTERFACE mode=s_axilite port=value_cache 						bundle=control
+	#pragma HLS INTERFACE mode=s_axilite port=key_cache								bundle=control
+	#pragma HLS INTERFACE mode=s_axilite port=value_cache_in 					bundle=control
+	#pragma HLS INTERFACE mode=s_axilite port=key_cache_in						bundle=control
+	#pragma HLS INTERFACE mode=s_axilite port=CURR_LAYER 							bundle=control
+	#pragma HLS INTERFACE mode=s_axilite port=POS 										bundle=control
+	#pragma HLS INTERFACE mode=s_axilite port=return 									bundle=control
+	
+	s_mfdata_v_t xb_ws_q("WS to Quantizer for XB Stream");
+	s_mfdata_v_t abs_tokens, abs_max_raw_tok, max_tok_out;
+	hls::stream<my_float_t> q_max_val;
+	s_mfdata_v_t s_key_cache_to_kernel("From DDR to kernel key cache");
+	s_mfdata_v_t s_value_cache_to_kernel("From DDR to kernel value cache");
+	s_mfdata_v_t s_key_cache_in, s_query, s_value_cache_in, s_query_r, s_key_cache_in_r;
+
+  #pragma HLS STABLE variable=POS
+  #pragma HLS STABLE variable=CURR_LAYER
+
+	#pragma HLS STREAM variable=s_key_cache_in depth=MODEL_HEAD_SIZE * 2 / MAX_FL_ELEM  //good
+	#pragma HLS STREAM variable=s_key_cache_in_r depth=MODEL_HEAD_SIZE * 2 / MAX_FL_ELEM  //good
+	#pragma HLS STREAM variable=sf depth=MODEL_ELEMENTS / SM_FL_ELEM / MODEL_SCALING_FACTOR
+	#pragma HLS STREAM variable=s_value_cache_in depth=MODEL_HEAD_SIZE * 2 / MAX_FL_ELEM  //good
+	#pragma HLS STREAM variable=s_query depth=MODEL_HEAD_SIZE * 2 / MAX_FL_ELEM //good
+	#pragma HLS STREAM variable=s_query_r depth=MODEL_HEAD_SIZE * 2 / MAX_FL_ELEM //good
+	// #pragma HLS STREAM variable=xb_ws_q depth=8 //good
+	#pragma HLS STREAM variable=s_key_cache_to_kernel depth=4096 //good
+	#pragma HLS STREAM variable=s_value_cache_to_kernel depth=4096 //good
+	
+	#pragma HLS BIND_STORAGE variable=s_key_cache_in_r type=fifo impl=bram
+	#pragma HLS BIND_STORAGE variable=s_query type=fifo impl=bram
+	#pragma HLS BIND_STORAGE variable=s_query_r type=fifo impl=bram
+	#pragma HLS BIND_STORAGE variable=s_key_cache_to_kernel type=fifo impl=uram
+	#pragma HLS BIND_STORAGE variable=s_value_cache_to_kernel type=fifo impl=uram
+
+	mha_num_head_loop:
+	for (size_t i = 0; i < MODEL_NUM_HEADS; i++) {
+		#pragma HLS DATAFLOW
+		
+		hls::stream<my_float_t> mha_it_sm, att_sm_ws;
+		// s_mfdata_v_t xb;
+		// #pragma hls STREAM variable=xb depth = 64
+		#pragma HLS STREAM variable=mha_it_sm depth=512
+		#pragma HLS BIND_STORAGE variable=mha_it_sm type=fifo impl=bram
+		#pragma HLS STREAM variable=att_sm_ws depth=512
+		#pragma HLS BIND_STORAGE variable=att_sm_ws type=fifo impl=bram
+
+		mha_input_data(s_query_r, tokens, 0, i * MODEL_HEAD_SIZE); //read query first
+		mha_input_data(s_key_cache_in_r, tokens, 1, i * MODEL_HEAD_SIZE); //key
+		mha_input_data(s_value_cache_in, tokens, 2, i * MODEL_HEAD_SIZE); // value
+		rope_kernel<my_float_t, MAX_FL_ELEM, MODEL_HEAD_SIZE>(s_query, s_query_r, POS);
+		rope_kernel<my_float_t, MAX_FL_ELEM, MODEL_HEAD_SIZE>(s_key_cache_in, s_key_cache_in_r, POS);
+		mha_WAR_store_load(key_cache, s_key_cache_to_kernel, s_key_cache_in, CURR_LAYER, POS, i);
+		mha_WAR_store_load(value_cache, s_value_cache_to_kernel, s_value_cache_in, CURR_LAYER, POS, i);
+		wide_mha_iterate(mha_it_sm, s_query_r, s_key_cache_to_kernel, POS);
+		wide_mha_softmax(att_sm_ws, mha_it_sm, POS);
+		wide_mha_weighted_sum(xb_ws_q, att_sm_ws, s_value_cache_to_kernel, POS);
+		abs_intake(abs_max_raw_tok, abs_tokens, xb_ws_q);
+		max_finder(q_max_val, max_tok_out, abs_tokens, abs_max_raw_tok);
+		quant_out(sf, w, max_tok_out, q_max_val);
+		//quantizer goes here
+	}
+	
+	return;
+}
+
+void old_mha_kernel(mfdata_v_t *tokens, //6 mha_kernel
                 mfdata_v_t *key_cache, 
                 mfdata_v_t *value_cache, 
 								mfdata_v_t *key_cache_in, mfdata_v_t *value_cache_in,
@@ -325,4 +419,3 @@ void mha_kernel(mfdata_v_t *tokens, //6 mha_kernel
 
 	return;
 }
-
--- mha_cu/mha_forward.h
+++ mha_cu/mha_forward.h
@@ -172,6 +172,33 @@ void s2mm_output_data(hls::vector<T, N> *out, hls::stream<T> &in ,const size_t C
 	}
 }
 
+
+template<typename T, size_t N, size_t M>
+void mha_input_data(hls::stream<hls::vector<T, M>> &out, hls::vector<T, N> *in, const size_t CURR_LAYER, const int offset){
+	
+	const size_t COUNT = MODEL_ELEMENTS / N;
+	const int tot_off = CURR_LAYER * COUNT + (offset / N);
+	AXI4_to_STREAM:
+	for (int i = 0; i < (MODEL_HEAD_SIZE / M); i++) {
+		size_t idx = (M/N);
+		hls::vector<T, M> temp_m;
+		
+		for (int j = 0; j < idx; j++) {
+			#pragma HLS PIPELINE II=1
+			
+			size_t jdx = idx * i + j;
+			hls::vector<T, N> temp_n = in[jdx + tot_off];
+			
+			for (int k = 0; k < N; k++) {
+				size_t kdx = N * j + k;
+				temp_m[kdx] = temp_n[k];
+			}
+		}
+		out.write(temp_m);
+	}
+}
+
+
 template<typename T, int M>
 void s2mm_output_data(T *out, hls::stream<T> (&in)[M] ,const size_t COUNT, const size_t W_Off){
 	//remember to calculate W_Off before passing it here. T could be any size, lterally. 
@@ -294,6 +321,190 @@ void resid_conn(hls::stream<T> &tokens_out, hls::stream<T> &tokens_in, hls::stre
 
 	}
 }
+
+
+/* =============================== NEW MHA WRITE AFTER READ ======================================= */
+template<typename T, size_t N>
+void mha_WAR_store_load(hls::vector<T, N> *cache, hls::stream<hls::vector<T, N>> &output, hls::stream<hls::vector<T, N>> &input, const int CURR_LAYER, const int POS){
+	// const int num_heads = vSize / MODEL_HEAD_SIZE;
+	const int vec_per_head = MODEL_HEAD_SIZE / N;
+	const int cache_arr_size = vec_per_head * MODEL_NUM_HEADS;
+
+	const int layer_offset = CURR_LAYER * MODEL_NUM_HEADS * MODEL_SEQUENCE_LEN * vec_per_head;
+	const int head_offset = MODEL_SEQUENCE_LEN * vec_per_head;
+	const int pos_offset = POS * vec_per_head;
+	
+	hls::vector<T, N> cache_array[cache_arr_size];
+	mha_WAR_store_loop:
+	for (int i = 0;  i < cache_arr_size; i++) {
+		#pragma hls PIPELINE II=1
+		cache_array[i] = input.read();
+	}
+	const int vec_to_read = vec_per_head * (POS); // remove the + 1 from here.
+		
+	mha_num_head_loop:
+	for (int i = 0; i < MODEL_NUM_HEADS; i++) {
+		#pragma HLS LOOP_FLATTEN
+		fw_mha_pos_loop:
+		for (int j = 0; j < vec_to_read; j++) {
+			#pragma HLS PIPELINE II=1
+			#pragma HLS LOOP_TRIPCOUNT max=MODEL_HEAD_SIZE * (MODEL_SEQUENCE_LEN + 1) / MAX_FL_ELEM
+			int addr = layer_offset + (i * head_offset) + j;
+			hls::vector<T, N> tmp = cache[addr];
+			output.write(tmp);
+		} // second for loop that will read 4 elements from array
+		fw_mha_new_loop:
+		for (int j = 0; j < vec_per_head; j++) {
+			#pragma HLS PIPELINE II=1
+			int t = j + i * vec_per_head;
+			output.write(cache_array[t]);
+		}
+	}
+	
+	hls::fence(output, input);
+	
+	#pragma HLS STREAM variable=input depth=48
+	#pragma HLS STREAM variable=output depth=4
+	store_to_m_axi_loop: 
+	for (int i = 0; i < MODEL_NUM_HEADS; i++) {
+		for (int j = 0; j < vec_per_head; j++) {
+			#pragma HLS PIPELINE II=1
+			int addr = layer_offset + (i * head_offset) + pos_offset + j;
+			cache[addr] = cache_array[j + vec_per_head * i]; // this happens AFTER we're done reading from RAM
+		}
+	}
+}
+
+
+template<typename T, size_t N>
+void mha_WAR_store_load(hls::vector<T, N> *cache, hls::stream<hls::vector<T, N>> &output, hls::stream<hls::vector<T, N>> &input, const int CURR_LAYER, const int POS, const int idx){
+	// const int num_heads = vSize / MODEL_HEAD_SIZE;
+	const int vec_per_head = MODEL_HEAD_SIZE / N;
+	// const int cache_arr_size = vec_per_head * MODEL_NUM_HEADS;
+
+	const int layer_offset = CURR_LAYER * MODEL_NUM_HEADS * MODEL_SEQUENCE_LEN * vec_per_head;
+	const int head_offset = MODEL_SEQUENCE_LEN * vec_per_head;
+	const int pos_offset = POS * vec_per_head;
+	
+	hls::vector<T, N> cache_array[vec_per_head];
+	mha_WAR_store_loop:
+	for (int i = 0;  i < vec_per_head; i++) {
+		#pragma hls PIPELINE II=1
+		cache_array[i] = input.read();
+	}
+	const int vec_to_read = vec_per_head * (POS); // remove the + 1 from here.
+		
+	// mha_num_head_loop:
+	// for (int i = 0; i < MODEL_NUM_HEADS; i++) {
+		// #pragma HLS LOOP_FLATTEN
+		fw_mha_pos_loop:
+		for (int j = 0; j < vec_to_read; j++) {
+			#pragma HLS PIPELINE II=1
+			#pragma HLS LOOP_TRIPCOUNT max=MODEL_HEAD_SIZE * (MODEL_SEQUENCE_LEN + 1) / MAX_FL_ELEM
+			int addr = layer_offset + (idx * head_offset) + j;
+			hls::vector<T, N> tmp = cache[addr];
+			output.write(tmp);
+		} // second for loop that will read 4 elements from array
+		fw_mha_new_loop:
+		for (int j = 0; j < vec_per_head; j++) {
+			#pragma HLS PIPELINE II=1
+			int t = j + idx * vec_per_head;
+			output.write(cache_array[t]);
+		}
+	// }
+	
+	hls::fence(output, input);
+	
+	#pragma HLS STREAM variable=input depth=48
+	#pragma HLS STREAM variable=output depth=4
+	store_to_m_axi_loop: 
+	// for (int i = 0; i < MODEL_NUM_HEADS; i++) {
+		for (int j = 0; j < vec_per_head; j++) {
+			#pragma HLS PIPELINE II=1
+			int addr = layer_offset + (idx * head_offset) + pos_offset + j;
+			cache[addr] = cache_array[j + vec_per_head * idx]; // this happens AFTER we're done reading from RAM
+		}
+	// }
+}
+
+
+template<typename T, size_t N>
+void abs_intake(hls::stream<hls::vector<T, N>> &tokens_out, hls::stream<hls::vector<T, N>> &abs_tokens, hls::stream<hls::vector<T, N>> &tokens_in){
+	
+	const size_t TOK_COUNT = MODEL_SCALING_FACTOR / N;
+	T max_val = 0.0f;
+	
+	group_scaling:
+	for (size_t j = 0; j < TOK_COUNT; j++) {
+		#pragma HLS PIPELINE II=1
+		
+		hls::vector<T, N> val = tokens_in.read();
+		tokens_out.write(val);
+		
+		hls::vector<T, N> c_val;
+		for (int k = 0; k < N; k++) {
+			c_val[k] = hls::absf(val[k]);
+		}		
+		abs_tokens.write(c_val);
+	}
+	// can probably get rid of abs_intake
+}
+
+template<typename T, size_t N>
+void max_finder(hls::stream<T> &max_val, hls::stream<hls::vector<T, N>> &tokens_out, hls::stream<hls::vector<T, N>> &abs_tokens, hls::stream<hls::vector<T, N>> &tokens_in){
+	
+	const T Q_MAX = 1.0f / 127.0f;
+	const int cnt = MODEL_SCALING_FACTOR / N;
+	T c_val[MODEL_SCALING_FACTOR];
+	#pragma HLS ARRAY_PARTITION variable=c_val dim=1 type=complete
+	//here we store token_out and then assign token_out[i] to c_val[i * MAX_FL_ELEM + k] = hls::absf(token_out[i][k])
+	
+	
+	mf_intake:
+	for (int i = 0; i < cnt; i++) {
+		#pragma HLS PIPELINE II=1
+		hls::vector<T, N> val = abs_tokens.read();
+		tokens_out.write(tokens_in.read());
+		for (int k = 0; k < N; k++) {
+			c_val[i * N + k] = val[k];
+		}
+	}
+
+	for (int stride = (MODEL_SCALING_FACTOR>>1); stride > 0; stride >>=1) {
+		#pragma HLS UNROLL
+		for (int i = 0; i < stride; i++) {
+			#pragma HLS UNROLL
+			c_val[i] = (c_val[i]  > c_val[i + stride] ) ? c_val[i] : c_val[i + stride];
+		}
+	}	
+	max_val.write(c_val[0] * Q_MAX);
+}
+
+template<typename T, size_t N>
+void quant_out( hls::stream<T> &tok_sf_out, s_idata_v_t &tok_out, hls::stream<hls::vector<T, N>> &tokens_in, hls::stream<my_float_t> &max_val){
+	
+	const size_t TOK_COUNT = MODEL_SCALING_FACTOR / N;
+	T dscale = max_val.read(); 
+	T scale = hls::recipf(dscale);//Q_MAX / max_val;
+	idata_v_t quant_tmp; // not an array anymore
+	// fdata_v_t tok_arr[TOK_COUNT];
+	
+	create_q_val:
+	for (size_t j = 0; j < TOK_COUNT; j++) {
+		#pragma HLS PIPELINE
+		hls::vector<T, N> proc_tok = tokens_in.read();
+		
+		create_q_val_ppl:	
+		for (size_t k = 0; k < N; k++) {
+			#pragma HLS UNROLL factor = 4
+			quant_tmp[j * N + k] = (my_quant_data_t) hls::roundf(proc_tok[k] * scale);
+		}
+	}
+	
+	tok_out.write(quant_tmp);
+	tok_sf_out.write(dscale);
+}
+
 void mha_WAR_store_load(mfdata_v_t *cache, s_mfdata_v_t &output, s_mfdata_v_t &input, const int CURR_LAYER, const int POS);
 void mm_tok_load_input(s_idata_v_t &out, idata_v_t *in, const int vCount, const int CURR_LAYER);
 void mm_load_input(s_fdata_v_t &out, fdata_v_t *in, const int vCount, const int CURR_LAYER);
