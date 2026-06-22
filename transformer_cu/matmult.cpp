ac1297ef294b096fe8eafa9a91a0d5db0fa0b26a
marky fedora <workemail@company.com>
2026-06-21 19:39:33 -0500

    updated GEMV to get a big chuck of sf and to use uram instead of bram

--- transformer_cu/matmult.cpp
+++ transformer_cu/matmult.cpp
@@ -95,8 +95,8 @@ void GeMV_kernel(hls::stream<my_float_t> &out, s_fdata_v_t &tok_sf, s_idata_v_t 
 	s_fdata_v_t tokens("tokens");
 	#pragma HLS STREAM variable=tokens depth = 16// MODEL_HIDDEN_DIM/MAX_FL_ELEM
 	s_fdata_v_t s_wsf("s_wsf");
-#pragma HLS BIND_STORAGE variable=s_wsf type=fifo impl=bram
-	#pragma HLS STREAM variable=s_wsf type=fifo depth=16
+#pragma HLS BIND_STORAGE variable=s_wsf type=fifo impl=uram
+	#pragma HLS STREAM variable=s_wsf type=fifo depth=RECT_SF
 	s_idata_v_t s_w("s_w");
 #pragma HLS BIND_STORAGE variable=s_w type=fifo impl=bram
 	#pragma HLS STREAM variable=s_w type=fifo depth=16
